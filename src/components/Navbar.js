5d24aecac140b35b1512a6f8c7e430daa72a5612
Sebastian Valarezo <sebastianvalarezomariscal2004@gmail.com>
2021-03-17 20:02:02 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img
+        src="https://raw.githubusercontent.com/PokeAPI/media/master/logo/pokeapi_256.png"
+        alt="logo-pokedexapp"
+        className="navbar-logo"
+      ></img>
+    </nav>
+  );
+};
+
+export default Navbar;
